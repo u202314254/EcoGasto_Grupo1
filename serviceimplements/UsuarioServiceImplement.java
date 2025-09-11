0c5149284934675f202377b3882743533bdccbe0
AaronSLG <U202317813@upc.edu.pe>
2025-09-11 01:35:43 -0500

    Creacion de ServiceImplement

---
+++ serviceimplements/UsuarioServiceImplement.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.serviceimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.entities.Usuario;
+import pe.edu.upc.repositories.IUsuarioRepository;
+import pe.edu.upc.serviceinterfaces.IUsuarioService;
+
+import java.util.List;
+@Service
+public class UsuarioServiceImplement implements IUsuarioService {
+    @Autowired
+    private IUsuarioRepository uS;
+
+    @Override
+    public List<Usuario> list() {return uS.findAll(); }
+
+    @Override
+    public void insert(Usuario usuario) {uS.save(usuario);}
+}
