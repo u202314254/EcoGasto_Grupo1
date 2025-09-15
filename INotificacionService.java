31c9a9548ff3b0d2f33dd4402730420fce2cc3dd
Mauricio Mantilla <malexandromc@gmail.com>
2025-09-15 16:34:36 -0500

    Creación de la tabla Notificacion
    Con todas las funciones CRUD

---
+++ INotificacionService.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.serviceinterfaces;
+
+import pe.edu.upc.entities.Notificacion;
+import pe.edu.upc.entities.Perfil;
+
+import java.util.List;
+
+public interface INotificacionService {
+    public List<Notificacion> list();
+    public void insert(Notificacion notificacion);
+    public Notificacion listId(int id);
+    public void delete(int id);
+    public void update(Notificacion notificacion);
+}
