d5a641d4d2eadf473de5b4845e7b9d79872f1514
DilanAnton <u202312076@upc.edu.pe>
2025-09-16 00:29:12 -0500

    Creacion de toda la tabla Hogar, y correccion de los atributos de la tabla Usuario.

--- src/main/java/pe/edu/upc/UsuarioDTO.java
+++ src/main/java/pe/edu/upc/UsuarioDTO.java
@@ -1,36 +1,13 @@
-package pe.edu.upc.entities;
+package pe.edu.upc.dtos;
 
-import jakarta.persistence.*;
+import pe.edu.upc.entities.Hogar;
 
-@Entity
-@Table(name = "Usuario")
-
-public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class UsuarioDTO {
     private int idUsuario;
-
-    @ManyToOne
-    @JoinColumn(name = "idHogar", nullable = false)
     private Hogar hogar;
-
-    @Column(name = "correo", length = 50, nullable = false)
     private String correo;
-
-    @Column(name = "password", length = 50, nullable = false)
     private String password;
-
-    @Column(name = "estado", nullable = false)
     private boolean estado;
-    public Usuario() {}
-
-    public Usuario(int idUsuario, Hogar hogar, String correo, String password, boolean estado) {
-        this.idUsuario = idUsuario;
-        this.hogar = hogar;
-        this.correo = correo;
-        this.password = password;
-        this.estado = estado;
-    }
 
     public int getIdUsuario() {
         return idUsuario;
