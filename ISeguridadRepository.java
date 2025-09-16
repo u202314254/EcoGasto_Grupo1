e700d5bd339db35691ff5a459e04e0c5cc14387e
Dot22-maker <matiasivanvr@gmail.com>
2025-09-15 23:04:52 -0500

    Revert "entities, repositorie y dto de tabla seguridad"
    This reverts commit 0a918c632ff5c1dbe443ce8d19d04579023ffea6.

--- ISeguridadRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.entities.Seguridad;
-
-public interface ISeguridadRepository extends JpaRepository<Seguridad, Integer> {
-}
