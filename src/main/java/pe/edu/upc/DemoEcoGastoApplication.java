387afeb70093df0709baf86377ef733298ef0622
Mauricio Mantilla <malexandromc@gmail.com>
2025-10-02 22:20:24 -0500

    Update DemoEcoGastoApplication.java

--- src/main/java/pe/edu/upc/DemoEcoGastoApplication.java
+++ src/main/java/pe/edu/upc/DemoEcoGastoApplication.java
@@ -2,7 +2,17 @@ package pe.edu.upc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+        info = @Info(title = "Mi API", version = "1.0"),
+        security = { @SecurityRequirement(name = "bearerAuth") }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 @SpringBootApplication
 public class DemoEcoGastoApplication {
 
