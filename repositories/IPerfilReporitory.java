51c49cab36a037f063d2991575ba37227172f503
Mauricio Mantilla <malexandromc@gmail.com>
2025-10-02 22:23:19 -0500

    Agrego maven wrapper
    y demás archivos del backend

--- repositories/IPerfilReporitory.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.entities.Perfil;
-@Repository
-
-public interface IPerfilReporitory extends JpaRepository<Perfil,Integer> {
-}
