fa5433a04a6c9a1e4e090f419dbf32326891eaef
Mauricio Mantilla <malexandromc@gmail.com>
2025-10-02 22:20:46 -0500

    Update DemoEcoGastoApplication.java

--- src/main/java/pe/edu/upc/DemoEcoGastoApplication.java
+++ src/main/java/pe/edu/upc/DemoEcoGastoApplication.java
@@ -1,5 +1,11 @@
 package pe.edu.upc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(
